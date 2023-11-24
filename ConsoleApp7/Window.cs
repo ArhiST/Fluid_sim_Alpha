ee439c51f31ee85204064280b52212b04d851ec1
Ultrabook-13 <Ultrabook-13@DESKTOP-N9NGV6G>
2023-11-24 15:45:50 +0300

    <>

--- ConsoleApp7/Window.cs
+++ ConsoleApp7/Window.cs
@@ -5,20 +5,27 @@ using OpenTK.Mathematics;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.GraphicsLibraryFramework;
+using System.Reflection;
 
 
 namespace ConsoleApp7
 {
     internal class Window : GameWindow
     {
-        static int Segments = 14;
-        private float[] _verticies = CreateN.SphereCreator(Segments, 0.25f);        
+        static int Segments = 32;
+        private float[] _verticies = CreateN.SphereCreator(Segments, 0.45f);        
 
         private readonly uint[] _indices = CreateN.SpherePolygonsCreator(Segments);      
        
         private int _vertexBufferObject;
         private int _vertexArrayObject;
         private int _elementBufferObject;
+
+        private double _time;
+
+        private Matrix4 _projection; // 
+        private Matrix4 _view; //
+
         private Stopwatch _timer;
         private Shader _shader;
         public Window(GameWindowSettings Settings, NativeWindowSettings nativeWindowSettings)
@@ -33,7 +40,8 @@ namespace ConsoleApp7
         protected override void OnLoad()
         {
             base.OnLoad();
-            GL.ClearColor(0f, 0f, 0.2f, 0f);
+            GL.ClearColor(0f, 0f, 0.3f, 1f);
+            GL.Enable(EnableCap.DepthTest);
             _vertexBufferObject = GL.GenBuffer();
             GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
             GL.BufferData(BufferTarget.ArrayBuffer, _verticies.Length * sizeof(float), _verticies, BufferUsageHint.StaticDraw);
@@ -47,25 +55,34 @@ namespace ConsoleApp7
             GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);            
             GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
 
-            _shader = new Shader("C:\\Users\\Stepan\\source\\repos\\ConsoleApp7\\shader.vert", "C:\\Users\\Stepan\\source\\repos\\ConsoleApp7\\shader.frag");
+            _shader = new Shader("C:\\Users\\Ultrabook-13\\source\\repos\\ArhiST\\Fluid_sim_Alpha\\shader.vert",
+                "C:\\Users\\Ultrabook-13\\source\\repos\\ArhiST\\Fluid_sim_Alpha\\shader.frag");
             _shader.Use();
 
             _timer = new Stopwatch();
             _timer.Start();
+
+            _view = Matrix4.CreateTranslation(0f, 0f, -3f);
+            _projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45f), Size.X / (float)Size.Y, 0.1f, 100f);
         }
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             base.OnRenderFrame(e);
-            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            _time = e.Time;
             double TimeValue = _timer.Elapsed.TotalSeconds;
             var transform = Matrix4.Identity;
+            transform = transform * Matrix4.CreateRotationX((float)_time);
             //Vector3 Velocity = (0f, -0.981f *  (float)TimeValue, 0f);
-            
 
+            var model = Matrix4.Identity * Matrix4.CreateTranslation(0.05f, 0.05f*(float)TimeValue, 0f);
             //transform = transform * Matrix4.CreateRotationY(MathHelper.DegreesToRadians(70f));
             //transform = transform * Matrix4.CreateTranslation(Velocity);
 
-            _shader.SetMatrix4("transform", transform);
+            //_shader.SetMatrix4("transform", transform);
+            _shader.SetMatrix4("model", model);
+            _shader.SetMatrix4("view", _view);
+            _shader.SetMatrix4("projection", _projection);
 
             _shader.Use();                       
             float ColorValue = (float)Math.Sin(TimeValue);            
--- shader.vert
+++ shader.vert
@@ -3,12 +3,15 @@
 layout(location = 0) in vec3 aPosition;  
 
 
-uniform mat4 transform;
- 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+
 
 void main(void)
 {	
-    gl_Position = vec4(aPosition, 1.0) * transform; 
+    gl_Position = vec4(aPosition, 1.0) * model * view * projection; 
 
 	
 }
